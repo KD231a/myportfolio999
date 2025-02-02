ec827ff23841edb033939d83745c40ef08b0894b
PraiwanX <praiwansunanta123@gmail.com>
2025-02-02 15:22:31 +0700

    Deploy auto-update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/myportforlio999/",
+  base: "/myportfolio999/",  // Corrected typo here
 })
